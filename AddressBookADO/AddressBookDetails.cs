bd5f56c2d981a50ae5ccd4895fb994a72472137e
[Sona] <[sona16061999@gmail.com]>
2021-08-06 14:40:19 +0530

    [Sona] DB is created using Sql client

---
+++ AddressBookADO/AddressBookDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace AddressBookADO
+{
+    class AddressBookDetails
+    {
+        public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Address_Book;Integrated Security=True;";
+        SqlConnection sqlConnection = new SqlConnection(connectionString);
+    }
+}
